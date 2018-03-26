84d9239b1a6fd48cbe2e449ccbc132d2dbab91d5
William Jones <billy@freshjones.com>
2018-03-26 15:58:04 -0400

    working

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 'use strict';
 
 var gulp              = require('gulp');
-
+var sass              = require('gulp-sass');
+var sourcemaps        = require('gulp-sourcemaps');
+var fileinclude       = require('gulp-file-include');
+var rename            = require("gulp-rename");
 var browserSync       = require('browser-sync').create();
 var reload            = browserSync.reload;
+var del               = require('del');
 
 gulp.task('browsersync', function() {
     browserSync.init({
@@ -12,8 +16,22 @@ gulp.task('browsersync', function() {
     });
 });
 
+gulp.task('clean', function () {
+    del.sync(['./dist/**', '!./dist']);
+});
+
 gulp.task('html', function() {
-    gulp.src('./src/**/*.html')
+    gulp.src('./src/**/*.tpl.html')
+        .pipe(fileinclude({
+          prefix: '@@',
+          basepath: '@root'
+        }))
+        .pipe(rename({
+          extname: ""
+        }))
+        .pipe(rename({
+          extname: ".html"
+        }))
         .pipe(gulp.dest('./dist'));
 });
 
@@ -23,7 +41,7 @@ gulp.task('images', function() {
 });
 
 gulp.task('css', function () {
-   gulp.src('./src/css/main.css')
+   gulp.src('./src/css/styles.css')
        .pipe(gulp.dest('./dist/css'))
 });
 
@@ -32,13 +50,24 @@ gulp.task('js', function () {
        .pipe(gulp.dest('./dist/js'))
 });
 
-gulp.task('watch', ['browsersync'], function () 
+gulp.task('scss', function () 
 {
-  gulp.watch(['./src/**/*.html'], reload)
+  gulp.src('./src/scss/styles.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'expanded' }))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('./src/css'))
+    .pipe(browserSync.stream({match: '**/*.css'}));
+});
+
+gulp.task('watch', ['scss','browsersync'], function () 
+{
+  gulp.watch(['./src/**/*.html'], ['html']).on('change', reload);
+  gulp.watch(['./src/scss/**/*.scss'], ['scss']);
 });
 
 // Gulp default task
-gulp.task('build', ['html','images','css','js']);
+gulp.task('build', ['clean', 'html','images','css','js']);
 
 gulp.task('default', ['watch']);
 
