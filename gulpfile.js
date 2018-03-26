8640ba0dd9682eeccd1411a33c99d30dea1b4f06
William Jones <billy@freshjones.com>
2018-03-26 13:23:11 -0400

    set up gulp

--- gulpfile.js
+++ gulpfile.js
@@ -17,18 +17,28 @@ gulp.task('html', function() {
         .pipe(gulp.dest('./dist'));
 });
 
+gulp.task('images', function() {
+    gulp.src('./src/images/**/*')
+        .pipe(gulp.dest('./dist/images'));
+});
+
 gulp.task('css', function () {
    gulp.src('./src/css/main.css')
        .pipe(gulp.dest('./dist/css'))
 });
 
+gulp.task('js', function () {
+   gulp.src('./src/js/main.js')
+       .pipe(gulp.dest('./dist/js'))
+});
+
 gulp.task('watch', ['browsersync'], function () 
 {
-  gulp.watch(['./**/*.html'], reload)
+  gulp.watch(['./src/**/*.html'], reload)
 });
 
 // Gulp default task
-gulp.task('build', ['html','css']);
+gulp.task('build', ['html','images','css','js']);
 
 gulp.task('default', []);
 
