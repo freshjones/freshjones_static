87f5aa9d12c5abdf3c146f67edd3de10be83e3ac
William Jones <billy@freshjones.com>
2018-04-04 15:02:02 -0400

    workign

---
+++ src/js/subnav.js
@@ -0,0 +1,21 @@
+(function ($) {
+  $(function() {
+    var timeout;
+    $('.show-subnav')
+      .mouseleave(function() {
+        var _this = $(this);
+        timeout = setTimeout(function(){ 
+          _this.find('.subnav-panel').removeClass('subnav-panel-active');
+        }, 200);
+      })
+      .mouseenter(function() {
+        clearTimeout(timeout);
+        $(this).find('.subnav-panel').addClass('subnav-panel-active');
+      })
+      .click(function(e){
+        e.preventDefault();
+        clearTimeout(timeout);
+      })
+
+  });
+}(jQuery));
