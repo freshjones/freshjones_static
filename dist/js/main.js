5fcda284e5795fc3cf0e2575660479a126e58e66
William Jones <billy@freshjones.com>
2018-04-06 08:04:10 -0400

    working

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,78 @@
-!function(n){n(function(){n(".internal-notes .icon").on("click",function(){n(this).siblings().toggleClass("contents-active")})})}(jQuery),function(n){n(function(){n(".menu-toggle, .mobile-nav .toggle").on("click",function(){n("body").toggleClass("show-mobile-nav")})})}(jQuery);var scrollFunctions={};!function(t){scrollFunctions.visible=function(){for(var n=t(document).scrollTop()+t(window).height(),o=t("main section"),i=0;i<o.length;i++){var e=o[i];t(e).position().top<n?t(e).addClass("visible"):t(e).removeClass("visible")}}}(jQuery),$(function(){scrollFunctions.visible(),$(document).on("scroll",function(){scrollFunctions.visible()})}),function(i){i(function(){var o;i(".show-subnav").mouseleave(function(){var n=i(this);o=setTimeout(function(){n.find(".subnav-panel").removeClass("subnav-panel-active")},200)}).mouseenter(function(){clearTimeout(o),i(this).find(".subnav-panel").addClass("subnav-panel-active")}).click(function(n){n.preventDefault(),clearTimeout(o)})})}(jQuery);
+(function ($) {
+  $(function() {
+    $('.internal-notes .icon').on('click',function(){
+      //$('section').css("z-index", '20');
+      //$('.internal-notes .contents').removeClass('contents-active');
+      //$(this).parents('section').css("z-index", "100");
+      $(this).siblings().toggleClass('contents-active');
+    });
+  });
+}(jQuery));
+
+(function ($) {
+  $(function() {
+    $('.menu-toggle, .mobile-nav .toggle').on('click',function(){
+      $('body').toggleClass('show-mobile-nav');
+    })
+  });
+}(jQuery));
+
+
+
+
+
+
+var scrollFunctions = {};
+
+(function($) {
+   
+  scrollFunctions.visible = function() 
+  {
+    var pageTop = $(document).scrollTop()
+    var pageBottom = pageTop + $(window).height()
+    var tags = $("main section")
+    for (var i = 0; i < tags.length; i++) {
+      var tag = tags[i]
+      if ($(tag).position().top < pageBottom) {
+        $(tag).addClass("visible")
+      } else {
+        $(tag).removeClass("visible")
+      }
+    }
+  };
+ 
+})(jQuery);
+
+$(function() {
+  scrollFunctions.visible();
+  $(document).on("scroll", function () {
+    scrollFunctions.visible();
+  })
+});
+
+
+
+
+
+
+(function ($) {
+  $(function() {
+    var timeout;
+    $('.show-subnav')
+      .mouseleave(function() {
+        var _this = $(this);
+        timeout = setTimeout(function(){ 
+          _this.find('.subnav-panel').removeClass('subnav-panel-active');
+        }, 200);
+      })
+      .mouseenter(function() {
+        clearTimeout(timeout);
+        $(this).find('.subnav-panel').addClass('subnav-panel-active');
+      })
+      .click(function(e){
+        e.preventDefault();
+        clearTimeout(timeout);
+      })
+
+  });
+}(jQuery));
