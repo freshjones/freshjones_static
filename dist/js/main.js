a7dfadb4f17cf5d4b8eb4846063798a48597e306
William Jones <billy@freshjones.com>
2018-04-11 07:41:45 -0400

    test

--- dist/js/main.js
+++ dist/js/main.js
@@ -55,6 +55,29 @@ $(function() {
 
 
 
+$(function () { 
+
+  var controller = new ScrollMagic.Controller();
+
+  // define movement of panels
+  var wipeAnimation = new TimelineMax()
+    .fromTo(".trigger-1", .2, {opacity: "0"}, {opacity: "1"}) 
+    .fromTo(".trigger-2", .2, {opacity:  "0"}, {opacity: "1"})
+    .fromTo(".trigger-3", .2, {opacity: "0"}, {opacity: "1"})
+    .fromTo(".trigger-4", .2, {opacity: "0"}, {opacity: "1"});
+
+  // create scene to pin and link animation
+  new ScrollMagic.Scene({
+      triggerElement: ".scrollTrigger",
+      triggerHook: "onLeave",
+      duration: "100%"
+    })
+    .setPin(".scrollTrigger")
+    .setTween(wipeAnimation)
+    .addIndicators()
+    .addTo(controller);
+});
+
 (function ($) {
   $(function() {
     var timeout;
