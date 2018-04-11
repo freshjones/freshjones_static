4dac6f70e36c3090baddb8b87af6bd9f2368e21f
William Jones <billy@freshjones.com>
2018-04-11 07:48:08 -0400

    test

--- dist/js/main.js
+++ dist/js/main.js
@@ -61,10 +61,10 @@ $(function () {
 
   // define movement of panels
   var wipeAnimation = new TimelineMax()
-    .fromTo(".trigger-1", .2, {opacity: "0"}, {opacity: "1"}) 
-    .fromTo(".trigger-2", .2, {opacity:  "0"}, {opacity: "1"})
-    .fromTo(".trigger-3", .2, {opacity: "0"}, {opacity: "1"})
-    .fromTo(".trigger-4", .2, {opacity: "0"}, {opacity: "1"});
+    .fromTo(".scroll-item-1", .2, {opacity: "0"}, {opacity: "1"}) 
+    .fromTo(".scroll-item-2", .2, {opacity:  "0"}, {opacity: "1"})
+    .fromTo(".scroll-item-3", .2, {opacity: "0"}, {opacity: "1"})
+    .fromTo(".scroll-item-4", .2, {opacity: "0"}, {opacity: "1"});
 
   // create scene to pin and link animation
   new ScrollMagic.Scene({
