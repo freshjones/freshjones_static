51a68db73e3243827ad465a3446a0aa2857a4cd9
William Jones <billy@freshjones.com>
2018-04-28 18:03:57 -0400

    workign

--- src/js/subnav.js
+++ src/js/subnav.js
@@ -1,3 +1,40 @@
+
+(function ($) {
+  $(function() {
+    /*
+    var timeout;
+    $('.show-subnav')
+      .click(function(e){
+        e.preventDefault();
+        clearTimeout(timeout);
+        $('.show-subnav').removeClass('show-subnav-active');
+        $(this).addClass('show-subnav-active');
+        $('.subnav-panel').removeClass('subnav-panel-active');
+        $(this).find('.subnav-panel').addClass('subnav-panel-active');
+      })
+    */
+
+    $('.show-subnav')
+      .mouseenter(function(e) {
+        $(this).addClass('show-subnav-active');
+        $(this).find('.subnav-panel').show();
+      })
+      .mouseleave(function(e) {
+        $(this).removeClass('show-subnav-active');
+        $(this).find('.subnav-panel').hide();
+      })
+
+    $('.subnav-drop-down-item')
+      .mouseenter(function() {
+        $(this).find('ul:first').show();
+      })
+      .mouseleave(function() {
+        $(this).find('ul:first').hide();
+      })
+  });
+}(jQuery));
+
+/*
 (function ($) {
   $(function() {
     var timeout;
@@ -20,3 +57,4 @@
 
   });
 }(jQuery));
+*/
